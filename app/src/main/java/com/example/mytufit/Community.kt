e44cabee7c90d8042e4a56535a6db40dec6559e3
Lawrence <B00153847@mytudublin.ie>
2024-12-12 18:09:14 +0000

    New Discussion for each button from Community Page

--- app/src/main/java/com/example/mytufit/Community.kt
+++ app/src/main/java/com/example/mytufit/Community.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 
 class CommunityFragment : Fragment() {
@@ -12,6 +13,44 @@ class CommunityFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_community, container, false)
+        val view = inflater.inflate(R.layout.fragment_community, container, false)
+
+        // Initialize buttons
+        val btnWeightLoss = view.findViewById<Button>(R.id.btnWeightLoss)
+        val btnWeightGain = view.findViewById<Button>(R.id.btnWeightGain)
+        val btnRunningGroup = view.findViewById<Button>(R.id.btnRunningGroup)
+        val btnBestSnacksForProtein = view.findViewById<Button>(R.id.btnBestSnacksForProtein)
+        val btnStretches = view.findViewById<Button>(R.id.btnStretches)
+        val btnGeneralChat = view.findViewById<Button>(R.id.btnGeneralChat)
+
+        // Set click listeners to navigate to respective fragments
+        btnWeightLoss.setOnClickListener {
+            navigateToFragment(WriteFragment.newInstance("Weight Loss"))
+        }
+        btnWeightGain.setOnClickListener {
+            navigateToFragment(WriteFragment.newInstance("Weight Gain"))
+        }
+        btnRunningGroup.setOnClickListener {
+            navigateToFragment(WriteFragment.newInstance("Running Group"))
+        }
+        btnBestSnacksForProtein.setOnClickListener {
+            navigateToFragment(WriteFragment.newInstance("Best Snacks for Protein"))
+        }
+        btnStretches.setOnClickListener {
+            navigateToFragment(WriteFragment.newInstance("Stretches"))
+        }
+        btnGeneralChat.setOnClickListener {
+            navigateToFragment(WriteFragment.newInstance("General Chat"))
+        }
+
+        return view
+    }
+
+    // Helper function to navigate to a new fragment
+    private fun navigateToFragment(fragment: Fragment) {
+        parentFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .addToBackStack(null)
+            .commit()
     }
 }
