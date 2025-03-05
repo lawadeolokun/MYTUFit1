ccd437af683e73bd9694b3834959d53db4c18f5f
Yurieb <B00136730@mytudublin.ie>
2025-03-05 00:01:04 +0000

    added home screen and link to it from the Main activity

--- app/src/main/java/com/example/mytufit/HomeFragment.kt
+++ app/src/main/java/com/example/mytufit/HomeFragment.kt
@@ -1,2 +1,10 @@
 package com.example.mytufit
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
